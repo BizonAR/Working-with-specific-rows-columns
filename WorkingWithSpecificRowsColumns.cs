44864ca0c30b88e65b6d85f2abd9dd3306fda14f
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 11:56:12 +0500

    Working with specific rows/columns

---
+++ WorkingWithSpecificRowsColumns.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace WorkingWithSpecificRowsColumns
+{
+	internal class WorkingWithSpecificRowsColumns
+	{
+		static void Main(string[] args)
+		{
+			int[,] matrix = new int[4, 4];
+
+			Random random = new Random();
+			int maxValue = 101;
+
+			int sumSecondLine = 0;
+			int productFirstColumn = 1;
+
+			int secondLine = 1;
+			int firstColumn = 0;
+
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					matrix[i, j] = random.Next(maxValue);
+				}
+			}
+
+			for (int i = 0; i < matrix.GetLength(1); i++)
+			{
+				sumSecondLine += matrix[secondLine, i];
+			}
+
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				productFirstColumn *= matrix[i, firstColumn];
+			}
+
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					Console.Write(matrix[i, j] + "  ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine($"Сумма второй строки: {sumSecondLine}, произведение первого столбца: {productFirstColumn}");
+		}
+	}
+}
